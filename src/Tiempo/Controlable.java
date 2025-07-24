3a6064418293c8259b015c1631964c58a1ff1f3a
Jeshuan29 <jetorresgo@est.utn.ac.cr>
2025-07-24 16:20:15 -0600

    Interfaz Controlable

---
+++ src/Tiempo/Controlable.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Tiempo;
+
+/**
+ *
+ * @author Jeshuan
+ */
+public interface Controlable {
+  public void inciar();
+  public void detener();
+}
