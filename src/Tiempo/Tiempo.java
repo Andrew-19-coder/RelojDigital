3407038c5415902860964000eba5256c2a312076
Jeshuan29 <jetorresgo@est.utn.ac.cr>
2025-07-24 15:52:43 -0600

    Clase Abstracta Tiempo

--- src/Tiempo/Tiempo.java
+++ src/Tiempo/Tiempo.java
@@ -8,6 +8,69 @@ package Tiempo;
  *
  * @author itsth
  */
-public class Tiempo {
+public abstract class Tiempo {
+    private int horas;
+    private int segundos;
+    private int minutos;
+   
+    
+    public Tiempo() {
+        this.horas = 0;
+        this.segundos = 0;
+        this.minutos = 0;
+    }
+    public void correr(){
+        segundos ++;
+        if(segundos==60){
+            segundos=0;
+            minutos++;
+            if(minutos==60){
+            minutos=0;
+            horas++;
+            if(horas==24){
+            horas=0;
+            }
+          }
+        }
+     }
+    public int getHoras() {
+        return horas;
+    }
+
+    public int getSegundos() {
+        return segundos;
+    }
+
+    public int getMinutos() {
+        return minutos;
+    }
+
+    public void setHoras(int horas) {
+        this.horas = horas;
+    }
+
+    public void setSegundos(int segundos) {
+        this.segundos = segundos;
+    }
+
+    public void setMinutos(int minutos) {
+        this.minutos = minutos;
+    }
+    public String toString(){
+        String h, m, s;
+        h=String.valueOf(horas);
+        if(horas<10){
+        h="0".concat(h);  
+    }
+        m=String.valueOf(minutos);
+         if(minutos<10){
+         m="0".concat(m);
+    }
+         s=String.valueOf(segundos);
+         if(segundos<10){
+        s="0".concat(s);
+    }
+         return h+":"+m+":"+s;
+    }
     
 }
